357b3d4cfbf8b1e2bd888a6e458bab1421161088
Marianna Costanza <mcostanza98@gmail.com>
2024-05-09 13:03:04 +0200

    actualizacion

--- src/components/hero/HeroAgency.jsx
+++ src/components/hero/HeroAgency.jsx
@@ -20,47 +20,46 @@ const HeroAgency = () => {
         <div className="ptf-showcase-image image-3">
           <div className="ptf-showcase-slide">
             <div className="container">
-              <h1
-                className="fz-100 fz-100--sm lh-1 has-white-color"
-                style={{ marginLeft: "2.5rem" }}
-              >
-                <span className="has-accent-1">Fundacion</span> <br />
-                Disgrup
-              </h1>
+              <div className="row">
+                <div className="col-lg-11">
+                  <h1
+                    className="fz-100 fz-100--sm lh-1 has-white-color"
+                    style={{ marginLeft: "2.5rem" }}
+                  >
+                    <span className="has-accent-1">Fundacion</span> <br />
+                    Disgrup
+                  </h1>
 
-              {/* <!--Spacer--> */}
-              <div
-                className="ptf-spacer"
-                style={{ "--ptf-xxl": "2.125rem" }}
-              ></div>
-              <p
-                className="fz-24 has-white-color"
-                style={{ maxWidth: "34.375rem", marginLeft: "3rem" }}
-              >
-                Ayudamos a las personas con discapacidad
-              </p>
+                  {/* <!--Spacer--> */}
+                  <div
+                    className="ptf-spacer"
+                    style={{ "--ptf-xxl": "2.125rem" }}
+                  ></div>
+                  <p
+                    className="fz-24 has-white-color"
+                    style={{ maxWidth: "34.375rem", marginLeft: "3rem" }}
+                  >
+                    Ayudamos a las personas con discapacidad
+                  </p>
 
-              {/* <!--Spacer--> */}
-              <div
-                className="ptf-spacer"
-                style={{ "--ptf-xxl": "1.625rem" }}
-              ></div>
+                  {/* <!--Spacer--> */}
+                  <div
+                    className="ptf-spacer"
+                    style={{ "--ptf-xxl": "1.625rem" }}
+                  ></div>
 
-              <div className="d-flex "
-              
-              style={{ "marginLeft": "5.3rem" }}>
-                {/* <!--Button--> */}
-                <Link
-                  className="ptf-btn ptf-btn--primary"
-                  
-                style={{ "fontSize": "36px" }}
-                  to="/contact"
-                  target="_self"
-                >
-                  Dona
-                </Link>
+                  <div className="d-flex " style={{ marginLeft: "5.3rem" }}>
+                    {/* <!--Button--> */}
+                    <Link
+                      className="ptf-btn ptf-btn--primary"
+                      style={{ fontSize: "36px" }}
+                      to="/contact"
+                      target="_self"
+                    >
+                      Dona
+                    </Link>
 
-                {/*
+                    {/*
                 <div
                     className="ptf-video-button"
                     onClick={() => setOpen(true)}
@@ -79,7 +78,7 @@ const HeroAgency = () => {
                       </div>
                     </div>
                     */}
-                {/* <!--Button--> 
+                    {/* <!--Button--> 
                 <Link
                   className="ptf-btn ptf-btn--secondary"
                   to="/conocenos"
@@ -87,6 +86,41 @@ const HeroAgency = () => {
                 >
                   Conocenos
                 </Link>*/}
+                  </div>
+
+                  
+                  </div>
+                
+                <div className="col-lg-1" >
+
+                  <div className="carrousel-wrapper" style={{ width: "800px" , display: "inline-block" }}>
+                        <img
+                          style={{ maxWidth: "60px" }}
+                          alt="aef"
+                          src="https://pngimg.com/uploads/disabled/disabled_PNG8.png"
+                          />
+                        <img
+                          style={{ maxWidth: "60px", marginTop: "20px" }}
+                          alt="aef"
+                          src="https://th.bing.com/th/id/R.7109d9ada3c7245323c7648f9fdb6b7c?rik=WaWvmP4wCTnQCg&riu=http%3a%2f%2fcdn-01.media-brady.com%2fstore%2fstfr%2fmedia%2fcatalog%2fproduct%2fd%2fm%2fdmeu_pslc2_670_1_std.lang.all.png&ehk=zfbj9zE%2fFs5w6icSY%2b9TkSSyDDNI%2ffrC9liZx%2bY%2f608%3d&risl=&pid=ImgRaw&r=0"
+                        />
+                        <img
+                          style={{ maxWidth: "60px" , marginTop: "20px"}}
+                          alt="aef"
+                          src="https://th.bing.com/th/id/OIP.yGoK0gv0wh63HrkmxFndcgHaHa?rs=1&pid=ImgDetMain"
+                        />
+                        <img
+                          style={{ maxWidth: "60px", marginTop: "20px" }}
+                          alt="aef"
+                          src="https://w7.pngwing.com/pngs/117/374/png-transparent-intellectual-disability-international-day-of-disabled-persons-gizarteratzea-mentally-s-text-area-learning-disability-thumbnail.png"
+                        />
+                        <img
+                          style={{ maxWidth: "60px", marginTop: "20px" }}
+                          alt="aef"
+                          src="https://i.imgur.com/7oXavVK.jpeg"
+                        />
+                        </div>
+                    </div>
               </div>
             </div>
           </div>
