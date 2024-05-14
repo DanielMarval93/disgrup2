7ce4fb1e12e4dab8581e16109671f8d72020b1fd
Marianna Costanza <mcostanza98@gmail.com>
2024-05-14 21:56:13 +0200

    hoover

--- src/components/hero/HeroAgency.jsx
+++ src/components/hero/HeroAgency.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Slider from "react-slick";
+import Tooltip from "../Tooltip";
+
 
 const HeroAgency = () => {
   const settings = {
@@ -91,36 +93,42 @@ const HeroAgency = () => {
                   
                   </div>
                 
+                
                 <div className="col-lg-1" >
 
                   <div className="carrousel-wrapper" style={{ width: "800px" , display: "inline-block" }}>
-                        <img
+                    <Tooltip/>
+                        {/* <ImageWithTooltip 
                           style={{ maxWidth: "60px" }}
                           alt="aef"
                           src="https://pngimg.com/uploads/disabled/disabled_PNG8.png"
+                          tooltipText= "jfdskfdksjhfdskfsd"
                           />
-                        <img
+                        <ImageWithTooltip
                           style={{ maxWidth: "60px", marginTop: "20px" }}
                           alt="aef"
                           src="https://th.bing.com/th/id/R.7109d9ada3c7245323c7648f9fdb6b7c?rik=WaWvmP4wCTnQCg&riu=http%3a%2f%2fcdn-01.media-brady.com%2fstore%2fstfr%2fmedia%2fcatalog%2fproduct%2fd%2fm%2fdmeu_pslc2_670_1_std.lang.all.png&ehk=zfbj9zE%2fFs5w6icSY%2b9TkSSyDDNI%2ffrC9liZx%2bY%2f608%3d&risl=&pid=ImgRaw&r=0"
+                          tooltipText= "jfdskfdksjhfdskfsd"
                         />
-                        <img
+                        <ImageWithTooltip
                           style={{ maxWidth: "60px" , marginTop: "20px"}}
                           alt="aef"
                           src="https://th.bing.com/th/id/OIP.yGoK0gv0wh63HrkmxFndcgHaHa?rs=1&pid=ImgDetMain"
+                          tooltipText= "jfdskfdksjhfdskfsd"
                         />
-                        <img
+                        <ImageWithTooltip
                           style={{ maxWidth: "60px", marginTop: "20px" }}
                           alt="aef"
                           src="https://w7.pngwing.com/pngs/117/374/png-transparent-intellectual-disability-international-day-of-disabled-persons-gizarteratzea-mentally-s-text-area-learning-disability-thumbnail.png"
+                          tooltipText= "jfdskfdksjhfdskfsd"
                         />
-                        <img
-                          style={{ maxWidth: "60px", marginTop: "20px" }}
-                          alt="aef"
-                          src="https://i.imgur.com/7oXavVK.jpeg"
-                        />
-                        </div>
-                    </div>
+                        <ImageWithTooltip
+                        src="https://i.imgur.com/7oXavVK.jpeg" 
+                        alt="aef" 
+                        tooltipText="Texto de tooltip 5" 
+        /> */}
+      </div>
+    </div>
               </div>
             </div>
           </div>
