68fd2b90aad05258896f6a4140c74d9ccba175d0
Marianna Costanza <mcostanza98@gmail.com>
2024-05-26 20:09:56 +0200

    slider

--- src/components/Slider2.jsx
+++ src/components/Slider2.jsx
@@ -5,23 +5,23 @@ const Slider2 = () => {
     <div className="sliderFlanTwo sliderFlanBody">
       <div className="sliderFlanTwo-logos">
       <div className="sliderFlanTwo-logos-slide">
-          <img alt="logo" src="https://identitatcorporativa.gencat.cat/web/.content/Documentacio/descarregues/identificacio/BN/idb_c_blanc.png" />
-          <img alt="logo" src="https://ccfundacions.cat/wp-content/uploads/2019/05/logo-white.png" />
-          <img alt="logo" src="https://disgrup.org/wp-content/uploads/2023/02/carrusel-web-fundacion-500-%C3%97-250-px-5.svg" />
-          <img alt="logo" src="https://disgrup.org/wp-content/uploads/2023/02/carrusel-web-fundacion-500-%C3%97-250-px-6.svg" />
-          <img alt="logo" src="https://www.sanidad.gob.es/diseno/img/MSA.Gob-bn-positivo-gris.svg" />
-          <img alt="logo" src="https://disgrup.org/wp-content/uploads/2023/05/14-1024x390.png" />
-          <img alt="logo" src="https://upload.wikimedia.org/wikipedia/commons/0/00/Logotipo_del_Ministerio_de_Justicia.svg" />
+          <img alt="logo" src="https://identitatcorporativa.gencat.cat/web/.content/Documentacio/descarregues/identificacio/BN/idb_c_blanc.png" className="large-logo" />
+          <img alt="logo" src="https://ccfundacions.cat/wp-content/uploads/2019/05/logo-white.png" className="small-logo" />
+          <img alt="logo" src="https://disgrup.org/wp-content/uploads/2023/02/carrusel-web-fundacion-500-%C3%97-250-px-5.svg" className="aef-logo"/>
+          <img alt="logo" src="https://disgrup.org/wp-content/uploads/2023/02/carrusel-web-fundacion-500-%C3%97-250-px-6.svg" className="aef-logo"/>
+          <img alt="logo" src="https://www.sanidad.gob.es/diseno/img/MSA.Gob-bn-positivo-gris.svg" className="ecom-logo"/>
+          <img alt="logo" src="https://disgrup.org/wp-content/uploads/2023/05/14-1024x390.png" className="ecom-logo"/>
+          <img alt="logo" src="https://upload.wikimedia.org/wikipedia/commons/0/00/Logotipo_del_Ministerio_de_Justicia.svg" className="ecom-logo"/>
         </div>
 
         <div className="sliderFlanTwo-logos-slide">
-        <img alt="logo" src="https://identitatcorporativa.gencat.cat/web/.content/Documentacio/descarregues/identificacio/BN/idb_c_blanc.png" />
-          <img alt="logo" src="https://ccfundacions.cat/wp-content/uploads/2019/05/logo-white.png" />
-          <img alt="logo" src="https://disgrup.org/wp-content/uploads/2023/02/carrusel-web-fundacion-500-%C3%97-250-px-5.svg" />
-          <img alt="logo" src="https://disgrup.org/wp-content/uploads/2023/02/carrusel-web-fundacion-500-%C3%97-250-px-6.svg" />
-          <img alt="logo" src="https://www.sanidad.gob.es/diseno/img/MSA.Gob-bn-positivo-gris.svg" />
-          <img alt="logo" src="https://disgrup.org/wp-content/uploads/2023/05/14-1024x390.png" />
-          <img alt="logo" src="https://upload.wikimedia.org/wikipedia/commons/0/00/Logotipo_del_Ministerio_de_Justicia.svg" />
+        <img alt="logo" src="https://identitatcorporativa.gencat.cat/web/.content/Documentacio/descarregues/identificacio/BN/idb_c_blanc.png" className="large-logo" />
+          <img alt="logo" src="https://ccfundacions.cat/wp-content/uploads/2019/05/logo-white.png" className="small-logo" />
+          <img alt="logo" src="https://disgrup.org/wp-content/uploads/2023/02/carrusel-web-fundacion-500-%C3%97-250-px-5.svg" className="aef-logo"/>
+          <img alt="logo" src="https://disgrup.org/wp-content/uploads/2023/02/carrusel-web-fundacion-500-%C3%97-250-px-6.svg" className="aef-logo"/>
+          <img alt="logo" src="https://www.sanidad.gob.es/diseno/img/MSA.Gob-bn-positivo-gris.svg" className="ecom-logo"/>
+          <img alt="logo" src="https://disgrup.org/wp-content/uploads/2023/05/14-1024x390.png" className="ecom-logo"/>
+          <img alt="logo" src="https://upload.wikimedia.org/wikipedia/commons/0/00/Logotipo_del_Ministerio_de_Justicia.svg" className="ecom-logo"/>
         </div>
       </div>
     </div>
